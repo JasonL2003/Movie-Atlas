1648668faf4cf6d51b39a5ddd48ac59a5076deb1
Jason Lau <jason_lau@123.com>
2024-11-29 23:41:09 -0500

    server.js change

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,16 +1,24 @@
-import express from "express"
-import cors from "cors"
-import reviews from "./api/reviews.route.js"
+import express from "express";
+import cors from "cors";
+import reviews from "./api/reviews.route.js";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors({
+  origin: 'https://movie-search-vercel-client.vercel.app',  
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-app.use("/api/v1/reviews", reviews)
-app.use("*", (req, res) => res.status(404).json({error: "not found"}))
+app.use(express.json());
 
-export default app
+app.use("/api/v1/reviews", reviews);
+
+
+app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
+
+export default app;
+
 
 
 
