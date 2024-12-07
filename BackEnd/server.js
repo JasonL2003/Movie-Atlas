425aafd3356651772cd3d8dab1c66d7917ce3026
Jason Lau <jason_lau@123.com>
2024-12-07 17:58:45 -0500

    update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -22,7 +22,7 @@ import reviews from './api/reviews.route.js';
 const app = express();
 
 app.use(cors({
-  origin: '*', 
+  origin: 'https://movie-search-vercel-client.vercel.app', 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
