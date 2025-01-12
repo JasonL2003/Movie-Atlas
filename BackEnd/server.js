9c037d0d2dee76b8bf668d007d580c0d5825c659
Jason Lau <jason_lau@123.com>
2025-01-11 21:59:00 -0500

    finished local and vercel dev

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,41 +1,32 @@
-//LOCAL DEV - app.use(cors()) allows all origins
-/*
-import express from "express"
-import cors from "cors"
-import reviews from "./api/reviews.route.js"
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-app.use("/api/v1/reviews", reviews)
-app.use("*", (req, res) => res.status(404).json({error: "not found"}))
-
-export default app 
-*/
-
-//Vercel Code
-
 import express from 'express';
 import cors from 'cors';
 import reviews from './api/reviews.route.js';
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    'https://movie-search-vercel-client.vercel.app',
-    'https://movieatlas.vercel.app'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+const isProduction = process.env.NODE_ENV === 'production';
+
+//Allow specific origins for production when NODE_ENV is production set in vercel
+if (isProduction) {
+  app.use(
+    cors({
+      origin: [
+        'https://movie-search-vercel-client.vercel.app',
+        'https://movieatlas.vercel.app',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+  );
+} else {
+  //Allow all origins for local development when NODE_ENV is development
+  app.use(cors());
+}
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Server is running!'); 
+  res.send('Server is running!');
 });
 
 app.use('/api/v1/reviews', reviews);
@@ -43,14 +34,3 @@ app.use('/api/v1/reviews', reviews);
 app.use('*', (req, res) => res.status(404).json({ error: 'Route not found' }));
 
 export default app;
-
-
-
-
-
-
-
-
-
-
-
